191fabc6e37a6bbd9f8b455e54477b2451513f7f
EduardoWill <eduardo.william188@gmail.com>
2026-04-24 19:49:15 -0300

    feat: Salvando contatos no perfil do usuário

--- services/contatos.ts
+++ services/contatos.ts
@@ -1,17 +1,37 @@
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
+import { auth, db, } from "../src/models/firebaseConfig";
 
-import { db } from "../src/models/firebaseConfig";
-
-export const buscarUsuario = async(telefone: string) => {
-   const q = query(
+export const buscarUsuario = async (telefone: string) => {
+  const q = query(
     collection(db, "users"),
     where("telefone", "==", telefone)
-   );
-   const snapshot = await getDocs(q);
-   if (snapshot.empty){
-    return null; 
-   }
-   return snapshot.docs[0].data();
+  );
+
+  const snapshot = await getDocs(q);
+
+  if (snapshot.empty) {
+    return null;
+  }
+
+  return snapshot.docs[0].data();
 };
 
 
+export const salvarContato = async (nome: string, telefone: string) => {
+  const user = auth.currentUser;
+
+  if (!user) {
+    console.log("Usuário não logado");
+    return;
+  }
+
+  await addDoc(
+    collection(db, "users", user.uid, "contatos"),
+    {
+      nome: nome,
+      telefone: telefone,
+    }
+  );
+
+  console.log("Contato salvo!");
+};
